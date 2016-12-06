6e7aa7e55a4a06d3418ff5a6bb490dc9ff1cc732
James Carnegie <james@atomist.com>
2016-12-06 17:39:55 +0000

    Addition of license files now supported

--- .atomist/editors/AddLicenseFile.ts
+++ .atomist/editors/AddLicenseFile.ts
@@ -6,11 +6,13 @@ import {PathExpressionEngine, PathExpression} from '@atomist/rug/tree/PathExpres
 
 import {parameter, inject, parameters, tag, editor} from '@atomist/rug/support/Metadata'
 
-import * as yaml from '@types/js-yaml'
+import * as yaml from "js-yaml"
 
 class LicenseParams extends ParametersSupport {
-  @parameter({required: true, description: "The name of the license to add. ", displayName: "License Name", pattern: "@url", maxLength: 20})
+  @parameter({required: true, description: "The name of the license to add. ", displayName: "License Name", pattern: "^.*$", maxLength: 20})
   licenseName: string = null
+
+
 
 }
 //return true iff file is a license file
@@ -33,22 +35,26 @@ class AddLicenseFile implements ProjectEditor<LicenseParams> {
 
   edit(project: Project, @parameters("LicenseParams") params: LicenseParams) {
 
+    let licenseFileName = ".atomist/templates/" + params.licenseName + ".yml"
+    let licenseFile = project.backingArchiveProject().findFile(licenseFileName)
+    if(licenseFile == null){
+      throw Error(`Unable to find licenseFile: ${licenseFileName}`)
+    }
+    let strings = licenseFile.content().split("---")
+
+    let details = yaml.safeLoad(strings[1])
+
     let licenseFiles: File[] = project.files().filter(isLicense)
 
     if(licenseFiles.length < 1){
-      let yamls = yaml.loadAll(project.backingArchiveProject().findFile(params.licenseName + ".yml").content(), (t => console.log(t)))
-      console.log(yamls)
-    //  let yamls = jsyaml.loadAll()
-        //let filename = params.licenseName + ".yml"
-      //  let licenseYaml = jsyaml
-        // let editorProject = project.backingArchiveProject()
-        // editorProject.files().forEach(t => console.log(t.path()))
-        // let pe = new PathExpression<Project,Yml>(".//*[name='" + filename +"']->yml")
-        //
-        // let licenseYml: Yml = this.eng.scalar(editorProject, pe)
-
-        //project.copyEditorBackingFileOrFail(filename, "LICENSE")
-        return new Result(Status.Success, "License file added")
+      console.log("Adding LICENSE file...")
+      project.addFile("LICENSE",strings[2])
+    }else if(licenseFiles.length == 1){
+      console.log("Updating LICENSE file...")
+      licenseFiles[0].setContent(strings[2])
+    }else{
+      throw Error(`Found too many license files wasn't sure what to do`)
     }
+    return new Result(Status.Success, "License file added")
   }
 }
---
+++ README.md
@@ -0,0 +1,8 @@
+Manage licensing and copyright notices
+
+The .atomist/templates directory contains all the currently supported licenses.
+
+Supported operations:
+- AddLicenseFile \<licenseName - name of a file from .atomist/templates without the .yml\>
+
+e.g. rug edit -C /tmp AddLicenseFile licenseName="agpl-3.0"
