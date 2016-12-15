f7806570538f5ecaec2e5e5ea61c29b04cf817cc
David Dooling <david@atomist.com>
2016-12-15 16:02:06 -0600

    Shampoo for public release
    Get Rugs up to date with conventions.

--- .atomist/editors/AddLicenseFile.ts
+++ .atomist/editors/AddLicenseFile.ts
@@ -1,58 +1,83 @@
-import {Project, File, Yml} from '@atomist/rug/model/Core'
-import {ParametersSupport, Parameters} from '@atomist/rug/operations/Parameters'
-import {ProjectEditor} from '@atomist/rug/operations/ProjectEditor'
-import {Result,Status} from '@atomist/rug/operations/Result'
-import {PathExpressionEngine, PathExpression} from '@atomist/rug/tree/PathExpression'
+/*
+ * Copyright © 2016 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import {parameter, inject, parameters, tag, editor} from '@atomist/rug/support/Metadata'
+import { Project, File, Yml } from '@atomist/rug/model/Core'
+import { ParametersSupport, Parameters } from '@atomist/rug/operations/Parameters'
+import { ProjectEditor } from '@atomist/rug/operations/ProjectEditor'
+import { Result, Status } from '@atomist/rug/operations/Result'
+import { PathExpressionEngine, PathExpression } from '@atomist/rug/tree/PathExpression'
+
+import { parameter, inject, parameters, tag, editor } from '@atomist/rug/support/Metadata'
 
 import * as yaml from "js-yaml"
 
 class LicenseParams extends ParametersSupport {
-  @parameter({required: true, description: "The name of the license to add. ", displayName: "License Name", pattern: "^.*$", maxLength: 20})
-  licenseName: string = null
+    @parameter({
+        required: true,
+        description: "the name of the license to add to project",
+        displayName: "License Name",
+        validInput: "the name of a license template without the .yml extension, see https://github.com/atomist-rugs/licensing-editors/tree/master/.atomist/templates",
+        pattern: "^\\w[-\\w.]*$",
+        minLength: 1,
+        maxLength: 20,
+    })
+    license_name: string = null
 }
 
 //return true if file is a license file
 function isLicense(f: File) {
-  let path = f.path().toLowerCase()
-  return path == "license" || path == "license.txt" || path == "license.md";
+    let path = f.path().toLowerCase()
+    return path == "license" || path == "license.txt" || path == "license.md";
 }
 
 @editor("Add a license file to a project")
 @tag("license")
 @tag("licensing")
 @tag("copyright")
+@tag("documentation")
 class AddLicenseFile implements ProjectEditor<LicenseParams> {
 
-  private eng: PathExpressionEngine;
+    private eng: PathExpressionEngine;
 
-  constructor(@inject("PathExpressionEngine") _eng: PathExpressionEngine ){
-    this.eng = _eng;
-  }
-
-  edit(project: Project, @parameters("LicenseParams") params: LicenseParams) {
-
-    let licenseFileName = ".atomist/templates/" + params.licenseName + ".yml"
-    let licenseFile = project.backingArchiveProject().findFile(licenseFileName)
-    if(licenseFile == null){
-      throw Error(`Unable to find licenseFile: ${licenseFileName}`)
+    constructor( @inject("PathExpressionEngine") _eng: PathExpressionEngine) {
+        this.eng = _eng;
     }
-    let strings = licenseFile.content().split("---")
 
-    let details = yaml.safeLoad(strings[1])
+    edit(project: Project, @parameters("LicenseParams") params: LicenseParams) {
 
-    let licenseFiles: File[] = project.files().filter(isLicense)
+        let licenseFileName = ".atomist/templates/" + params.license_name + ".yml"
+        let licenseFile = project.backingArchiveProject().findFile(licenseFileName)
+        if (licenseFile == null) {
+            throw Error(`Unable to find licenseFile: ${licenseFileName}`)
+        }
+        let strings = licenseFile.content().split("---")
 
-    if(licenseFiles.length < 1){
-      console.log("  Adding LICENSE file...")
-      project.addFile("LICENSE",strings[2])
-    }else if(licenseFiles.length == 1){
-      console.log("  Updating LICENSE file...")
-      licenseFiles[0].setContent(strings[2])
-    }else{
-      throw Error(`Found too many license files wasn't sure what to do`)
+        let details = yaml.safeLoad(strings[1])
+
+        let licenseFiles: File[] = project.files().filter(isLicense)
+
+        if (licenseFiles.length < 1) {
+            console.log("  Adding LICENSE file...")
+            project.addFile("LICENSE", strings[2])
+        } else if (licenseFiles.length == 1) {
+            console.log("  Updating LICENSE file...")
+            licenseFiles[0].setContent(strings[2])
+        } else {
+            throw Error(`Found too many license files wasn't sure what to do`)
+        }
+        return new Result(Status.Success, "License file added")
     }
-    return new Result(Status.Success, "License file added")
-  }
 }
--- README.md
+++ README.md
@@ -1,12 +1,105 @@
-Manage licensing and copyright notices
+# Atomist 'licensing-editors'
 
-The .atomist/templates directory contains all the currently supported licenses.
+[![Build Status](https://travis-ci.org/atomist-rugs/licensing-editors.svg?branch=master)](https://travis-ci.org/atomist-rugs/licensing-editors)
+[![Slack Status](https://join.atomist.com/badge.svg)](https://join.atomist.com)
+
+This [Rug](http://docs.atomist.com/) archive has editors that manage
+license files and copyright notices in software projects.  License
+files come from http://choosealicense.com/ .
 
 Supported operations:
-- AddLicenseFile \<licenseName - name of a file from .atomist/templates without the .yml\>
+-  \<licenseName - name of a file from .atomist/templates without the .yml\>
 
 e.g. `rug edit -C /tmp AddLicenseFile licenseName="agpl-3.0"`
 
----
+## Rugs
 
-License files come from http://choosealicense.com/
+### AddLicenseFile
+
+The AddLicenseFile editor adds a license file to a project.  If a
+license file already exists in the project, it replaces it.  If more
+than one license file exists in the project, it refuses to do
+anything.
+
+#### Prerequisites
+
+Before running this Editor, you must have the following prerequisites
+satisfied.
+
+*   A project with zero or one license file
+
+#### Parameters
+
+To run this editor, you must supply the following parameters.
+
+*   `license_name`: The name of the license to add to the project.
+    Available licenses can be found in the Atomist [templates][]
+    directory.  The value provided for the `license_name` parameter
+    should be the name of one of the files in that directory, without
+    the `.yml` extension.
+
+[templates]: https://github.com/atomist-rugs/licensing-editors/tree/master/.atomist/templates
+
+#### Running
+
+Run it as follows:
+
+```
+$ cd to/your/project
+$ rug edit atomist-rugs:licensing-editors:AddLicenseFile \
+    license_name=mit
+```
+
+This will add the MIT license to the project.  If no license file is
+found in the project, the license will be put in a file named
+`LICENSE` at the root of the project.  If a file named `LICENSE`,
+`LICENSE.txt`, or `LICENSE.md` is found in the project (the search is
+case insensitive), the contents of that file will be replaced with the
+provided license.  If you like the changes, commit them.
+
+## Support
+
+General support questions should be discussed in the `#support`
+channel on our community slack team
+at [atomist-community.slack.com][slack].
+
+If you find a problem, please create an [issue][].
+
+[issue]: https://github.com/atomist-rugs/licensing-editors/issues
+
+## Development
+
+You can build, test, and install the project locally with
+the [Rug CLI][cli].  Before running the Rug CLI and after any changes
+to the `.atomist/package.json` file, you should ensure all of the
+dependencies are available locally by running `npm install`.
+
+[cli]: https://github.com/atomist/rug-cli
+
+```sh
+$ cd .atomist
+$ npm install
+$ rug test
+$ rug install
+```
+
+To create a new release of the project, simply push a tag of the form
+`M.N.P` where `M`, `N`, and `P` are integers that form the next
+appropriate [semantic version][semver] for release.  For example:
+
+```sh
+$ git tag -a 1.2.3
+```
+
+The Travis CI build (see badge at the top of this page) will
+automatically create a GitHub release using the tag name for the
+release and the comment provided on the annotated tag as the contents
+of the release notes.  It will also automatically upload the needed
+artifacts.
+
+---
+Created by [Atomist][atomist].
+Need Help?  [Join our Slack team][slack].
+
+[atomist]: https://www.atomist.com/
+[slack]: https://join.atomist.com/
